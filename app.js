ae7e692b36b46352eb8e428d98707a8e450713c8
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-05-17 13:34:19 +0100

    added endpoint for adding order to blockchain

--- app.js
+++ app.js
@@ -1,17 +1,44 @@
-var express = require('express');
-var app = express();
-const port = process.env.PORT || 8080;
+let express = require('express');
+const bodyParser = require('body-parser');
+let app = express();
 
 const blockchain = require('./utils/blockchain-helpers.js');
-
 const config = require('./config.json');
+
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "read", ["hello_world"])
         .then(json => res.send(json))
         .catch(error => {
             console.log(error);
-            res.send({ error: error.message});
+            res.send({ error: error.message });
+        });
+});
+
+app.post('/order', function (req, res) {
+    // [TODO] Schema validate
+    console.log("[HTTP] Request inbound: POST /order");
+    let order = req.body;
+
+    let orderArguments = [order.destination.recipient,
+        order.destination.address,
+        order.source.location,
+        order.transport.company,
+        JSON.stringify({ items: order.items})
+    ];
+
+    console.log("[INVOKE] Invoked addOrder on chaincode");
+    blockchain.invoke(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "addOrder", orderArguments)
+        .then(json => {
+            console.log("[INVOKE] Completed successfully");
+            res.send(json)
+        })
+        .catch(error => {
+            console.log(error);
+            res.send({ error: error.message });
         });
 });
 
