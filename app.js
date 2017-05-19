5fd3ee83dfe82a3774bdd5a8e4d3067cdac8f0a9
Daniel Cotton <dan@daniel-cotton.co.uk>
2017-05-19 14:46:05 +0100

    Added additional client/owner props to create-new-order

--- app.js
+++ app.js
@@ -34,8 +34,8 @@ let swaggerJSDoc = require('swagger-jsdoc');
 
 // Options for the swagger docs
 let options = {
-    swaggerDefinition: swaggerDefinition,
-    apis: ['app.js'] // Self documenting within code
+  swaggerDefinition: swaggerDefinition,
+  apis: ['app.js'] // Self documenting within code
 };
 
 // Initialize Swagger-jsdoc
@@ -51,20 +51,20 @@ swaggerSpec.definitions = schema.swaggerise(swaggerSpec.definitions, require("./
 */
 const port = process.env.PORT || 8080;
 const stateDescriptions = {
-    "CREATED": "Order has been created.",
-    "PENDING": "Order is pending picking",
-    "PICKED": "Order has been fully picked",
-    "PARTIALLY_PICKED": "Order has been partially picked",
-    "READY_FOR_TRANSIT": "Order is ready for collection",
-    "AWAITING_PICKUP": "Order is awaiting collection for delivery",
-    "ENROUTE": "Order has been collected and is enroute",
-    "DELAYED": "Order has been delayed",
-    "CANCELLED": "Order delivery has been cancelled",
-    "DELIVERED": "Order has been fully delivered",
-    "PARTIALLY_DELIVERED": "Order has been partially delivered",
-    "DELIVERY_CONFIRMED": "Delivery has been confirmed by the recipient",
-    "FAILURE": "Delivery failed",
-    "REJECTED": "Delivery was rejected by the recipient"
+  "CREATED": "Order has been created.",
+  "PENDING": "Order is pending picking",
+  "PICKED": "Order has been fully picked",
+  "PARTIALLY_PICKED": "Order has been partially picked",
+  "READY_FOR_TRANSIT": "Order is ready for collection",
+  "AWAITING_PICKUP": "Order is awaiting collection for delivery",
+  "ENROUTE": "Order has been collected and is enroute",
+  "DELAYED": "Order has been delayed",
+  "CANCELLED": "Order delivery has been cancelled",
+  "DELIVERED": "Order has been fully delivered",
+  "PARTIALLY_DELIVERED": "Order has been partially delivered",
+  "DELIVERY_CONFIRMED": "Delivery has been confirmed by the recipient",
+  "FAILURE": "Delivery failed",
+  "REJECTED": "Delivery was rejected by the recipient"
 }
 const sourceStates = ["CREATED", "PENDING", "PICKED", "PARTIALLY_PICKED", "READY_FOR_TRANSIT"];
 const transportStates = ["AWAITING_PICKUP", "ENROUTE", "DELAYED", "CANCELLED", "DELIVERED", "PARTIALLY_DELIVERED", "DELIVERY_CONFIRMED", "FAILURE", "REJECTED"];
@@ -76,8 +76,8 @@ app.use(bodyParser.json());
 app.use(expressMiddleware.allowedOrigins);
 
 app.get('/swagger.json', function (req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
 });
 
 /**
@@ -96,14 +96,14 @@ app.get('/swagger.json', function (req, res) {
  *         description: Error querying chaincode
  */
 app.get('/states', function (req, res) {
-    console.log("[HTTP] Request inbound: GET /states");
-    let allStates = [].concat(sourceStates, transportStates);
-    let statesWithMessage = allStates.map(state => ({
-        state,
-        description: stateDescriptions[state]
-    }));
-    console.log("[HTTP] Writing response: GET /states");
-    res.send(statesWithMessage);
+  console.log("[HTTP] Request inbound: GET /states");
+  let allStates = [].concat(sourceStates, transportStates);
+  let statesWithMessage = allStates.map(state => ({
+    state,
+    description: stateDescriptions[state]
+  }));
+  console.log("[HTTP] Writing response: GET /states");
+  res.send(statesWithMessage);
 });
 
 /**
@@ -122,34 +122,34 @@ app.get('/states', function (req, res) {
  *         description: Error querying chaincode
  */
 app.get('/orders', function (req, res) {
-    console.log("[HTTP] Request inbound: GET /orders");
-    console.log("[QUERY] Querying getAllOrders on chaincode");
-    blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "getAllOrders", [])
-        .then(json => {
-            console.log("[QUERY] Completed successfully");
-            let orders = JSON.parse(json.result.message).orders
-            console.log("[QUERY] Querying getOrderHistory for orders");
-            let orderPromises = orders.map((order, index) => {
-                return blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "getOrderHistory", [order.id])
-                    .then(json => {
-                        let history = JSON.parse(json.result.message).orderUpdates.map(update => {
-                            update.timestamp = date.parseBlockchainTimestamp(update.timestamp);
-                            return update;
-                        })
-                        orders[index] = Object.assign({}, order, { history: history });
-                    });
-            });
-            return Promise.all(orderPromises).then(() => orders);
-        })
-        .then(orders => {
-            console.log("[QUERY] Querying complete on getOrderHistory for orders");
-            res.send(orders);
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(503);
-            res.send({ error: error.message });
-        });
+  console.log("[HTTP] Request inbound: GET /orders");
+  console.log("[QUERY] Querying getAllOrders on chaincode");
+  blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "getAllOrders", [])
+    .then(json => {
+      console.log("[QUERY] Completed successfully");
+      let orders = JSON.parse(json.result.message).orders
+      console.log("[QUERY] Querying getOrderHistory for orders");
+      let orderPromises = orders.map((order, index) => {
+        return blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "getOrderHistory", [order.id])
+          .then(json => {
+            let history = JSON.parse(json.result.message).orderUpdates.map(update => {
+              update.timestamp = date.parseBlockchainTimestamp(update.timestamp);
+              return update;
+            })
+            orders[index] = Object.assign({}, order, { history: history });
+          });
+      });
+      return Promise.all(orderPromises).then(() => orders);
+    })
+    .then(orders => {
+      console.log("[QUERY] Querying complete on getOrderHistory for orders");
+      res.send(orders);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(503);
+      res.send({ error: error.message });
+    });
 });
 
 /**
@@ -177,27 +177,29 @@ app.get('/orders', function (req, res) {
  *         description: Error invoking chaincode
  */
 app.post('/order', validate({ body: schemas.orderCreateSchema }), function (req, res) {
-    console.log("[HTTP] Request inbound: POST /order");
-    let order = req.body;
+  console.log("[HTTP] Request inbound: POST /order");
+  let order = req.body;
 
-    let orderArguments = [order.destination.recipient,
+  let orderArguments = [order.destination.recipient,
     order.destination.address,
     order.source.location,
     order.transport.company,
-    JSON.stringify({ items: order.items })
-    ];
+    JSON.stringify({ items: order.items }),
+    order.detail.client,
+    order.detail.owner
+  ];
 
-    console.log("[INVOKE] Invoked addOrder on chaincode");
-    blockchain.invoke(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "addOrder", orderArguments)
-        .then(json => {
-            console.log("[INVOKE] Completed successfully");
-            res.send(json)
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(503);
-            res.send({ error: error.message });
-        });
+  console.log("[INVOKE] Invoked addOrder on chaincode");
+  blockchain.invoke(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "addOrder", orderArguments)
+    .then(json => {
+      console.log("[INVOKE] Completed successfully");
+      res.send(json)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(503);
+      res.send({ error: error.message });
+    });
 });
 
 /**
@@ -222,34 +224,34 @@ app.post('/order', validate({ body: schemas.orderCreateSchema }), function (req,
  *         description: Error querying chaincode
  */
 app.get('/order/:id', function (req, res) {
-    console.log("[HTTP] Request inbound: GET /order/" + req.params.id);
-    console.log("[QUERY] Querying getOrder on chaincode");
-    let order = {};
-    let orderRetrieval = blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "getOrder", [req.params.id])
-        .then(json => {
-            console.log("[QUERY] Completed getOrder successfully");
-            order = Object.assign({}, order, JSON.parse(json.result.message));
-        });
-    console.log("[QUERY] Querying getOrderHistory on chaincode");
-    let orderHistoryRetrieval = blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "getOrderHistory", [req.params.id])
-        .then(json => {
-            console.log("[QUERY] Completed getOrderHistory successfully");
-            let history = JSON.parse(json.result.message).orderUpdates.map(update => {
-                update.timestamp = date.parseBlockchainTimestamp(update.timestamp);
-                return update;
-            })
-            order = Object.assign({}, order, { history: history });
-        });
+  console.log("[HTTP] Request inbound: GET /order/" + req.params.id);
+  console.log("[QUERY] Querying getOrder on chaincode");
+  let order = {};
+  let orderRetrieval = blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "getOrder", [req.params.id])
+    .then(json => {
+      console.log("[QUERY] Completed getOrder successfully");
+      order = Object.assign({}, order, JSON.parse(json.result.message));
+    });
+  console.log("[QUERY] Querying getOrderHistory on chaincode");
+  let orderHistoryRetrieval = blockchain.query(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "getOrderHistory", [req.params.id])
+    .then(json => {
+      console.log("[QUERY] Completed getOrderHistory successfully");
+      let history = JSON.parse(json.result.message).orderUpdates.map(update => {
+        update.timestamp = date.parseBlockchainTimestamp(update.timestamp);
+        return update;
+      })
+      order = Object.assign({}, order, { history: history });
+    });
 
-    Promise.all([orderRetrieval, orderHistoryRetrieval]).then(() => {
-        console.log("[HTTP] Request outbound: GET /order/" + req.params.id);
-        res.send(order);
-    })
-        .catch(error => {
-            console.log(error);
-            res.status(503);
-            res.send({ error: error.message });
-        });
+  Promise.all([orderRetrieval, orderHistoryRetrieval]).then(() => {
+    console.log("[HTTP] Request outbound: GET /order/" + req.params.id);
+    res.send(order);
+  })
+    .catch(error => {
+      console.log(error);
+      res.status(503);
+      res.send({ error: error.message });
+    });
 });
 
 /**
@@ -282,52 +284,52 @@ app.get('/order/:id', function (req, res) {
  *         description: Error invoking chaincode
  */
 app.put('/order/:id/status', validate({ body: schemas.orderStatusUpdateSchema }), function (req, res) {
-    console.log("[HTTP] Request inbound: POST /order");
-    let stateRequest = req.body;
+  console.log("[HTTP] Request inbound: POST /order");
+  let stateRequest = req.body;
 
-    let statusType = "SOURCE";
-    if (transportStates.indexOf(stateRequest.to) >= 0) {
-        statusType = "TRANSPORT";
-    }
+  let statusType = "SOURCE";
+  if (transportStates.indexOf(stateRequest.to) >= 0) {
+    statusType = "TRANSPORT";
+  }
 
-    let orderArguments = [req.params.id,
-        statusType,
-    stateRequest.to,
-    stateRequest.comment || ""];
+  let orderArguments = [req.params.id,
+    statusType,
+  stateRequest.to,
+  stateRequest.comment || ""];
 
-    console.log("[INVOKE] Invoked updateOrderStatus on chaincode");
-    blockchain.invoke(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "updateOrderStatus", orderArguments)
-        .then(json => {
-            console.log("[INVOKE] Completed successfully");
-            res.send(json)
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(503);
-            res.send({ error: error.message });
-        });
+  console.log("[INVOKE] Invoked updateOrderStatus on chaincode");
+  blockchain.invoke(config.peers[0].endpoint, config.chaincodeHash, config.peers[0].user, "updateOrderStatus", orderArguments)
+    .then(json => {
+      console.log("[INVOKE] Completed successfully");
+      res.send(json)
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(503);
+      res.send({ error: error.message });
+    });
 });
 
 
 // Express Error Handling for Validation
 app.use((err, req, res, next) => {
-    let responseData;
+  let responseData;
 
-    if (err.name === 'JsonSchemaValidation') {
+  if (err.name === 'JsonSchemaValidation') {
 
-        console.log("[HTTP] Request JSON schema validation failed.");
+    console.log("[HTTP] Request JSON schema validation failed.");
 
-        res.status(400);
+    res.status(400);
 
-        responseData = {
-            "error": "Bad Request",
-            "validationErrors": err.validations
-        };
+    responseData = {
+      "error": "Bad Request",
+      "validationErrors": err.validations
+    };
 
-        res.json(responseData);
-    } else {
-        next(err);
-    }
+    res.json(responseData);
+  } else {
+    next(err);
+  }
 });
 
 app.listen(port);
